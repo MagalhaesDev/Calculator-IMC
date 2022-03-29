b925eca58c2a82161f67160fb919b3426f7ef25d
Mateus <mateusmagalhaesemidio@gmail.com>
2022-03-29 15:16:48 -0300

    first  commit

---
+++ assets/script/script.js
@@ -0,0 +1,120 @@
+// ==================  BOTÃO PARA ALTERAR A COR DO SITE =================
+
+function themeSwitcher() {
+  const circle = document.querySelector('.circle')
+  const body = document.querySelector('body')
+  const button = document.querySelector('#calc')
+  const h2 = document.querySelector('.form-container h2')
+  const header = document.querySelector('header')
+  const circleOne = document.querySelector('.circle-one')
+  const circleTwo = document.querySelector('.circle-two')
+
+  circle.addEventListener('click', e => {
+    e.target.classList.toggle('on-click')
+
+    if (e.target.classList.contains('on-click')) {
+      body.style.background = '#f3f3f3'
+      button.style.background = 'black'
+      h2.style.color = '#3f3f3f'
+      header.style.background = 'black'
+      circle.style.background = '#707070'
+      circleOne.style.background = 'black'
+      circleTwo.style.background = 'black'
+    } else {
+      body.style.background = '#bd5067'
+      button.style.background = '#662641'
+      h2.style.color = '#e2e2e2'
+      header.style.background = '#662641'
+      circle.style.background = '#bd5067'
+      circleOne.style.background = '#66264171'
+      circleTwo.style.background = '#66264171'
+    }
+  })
+}
+
+// ==================  INFORMAÇÃO DA INTERROGAÇÃO =================
+
+function interrogationInformation() {
+  const p = document.querySelector('.help p')
+  const interrogation = document.querySelector('.interrogation')
+
+  p.addEventListener('mouseover', () => {
+    interrogation.classList.toggle('on-interrogation')
+  })
+
+  p.addEventListener('mouseout', () => {
+    interrogation.classList.toggle('on-interrogation')
+  })
+}
+
+// ==================  CALCULO DO IMC =================
+
+receiveValueInputAndCalc = () => {
+  const weight = document.querySelector('form label input#weight')
+  const height = document.querySelector('form label input#height')
+  const form = document.querySelector('form')
+  const modal = document.querySelector('.modal')
+  const resetSite = document.querySelector('#reset-site')
+  const closeModal = document.querySelector('#close-modal')
+  const textPrince = document.querySelector('#result-msg h2')
+  const textSecondary = document.querySelector('#result-msg p')
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    let finishedResult = ''
+
+    const valueWeight = weight.value
+    const valueHeight = height.value
+
+    const result = valueWeight / valueHeight ** 2
+    finishedResult = result.toFixed(2)
+
+    if (finishedResult < 18.5) {
+      ;(textPrince.innerHTML = `Seu IMC é ${finishedResult}`),
+        (textSecondary.innerHTML = '(Abaixo do peso)')
+    } else if (finishedResult >= 18.5 && finishedResult <= 24.9) {
+      ;(textPrince.innerHTML = `Seu IMC é ${finishedResult}`),
+        (textSecondary.innerHTML = '(Peso normal)')
+    } else if (finishedResult >= 25 && finishedResult <= 29.9) {
+      ;(textPrince.innerHTML = `Seu IMC é ${finishedResult}`),
+        (textSecondary.innerHTML = '(Sobrepeso)')
+    } else if (finishedResult >= 30 && finishedResult <= 34.9) {
+      ;(textPrince.innerHTML = `Seu IMC é ${finishedResult}`),
+        (textSecondary.innerHTML = '(Obesidade grau 1)')
+    } else if (finishedResult >= 35 && finishedResult <= 39.9) {
+      ;(textPrince.innerHTML = `Seu IMC é ${finishedResult}`),
+        (textSecondary.innerHTML = '(Obesidade grau 2)')
+    } else if (finishedResult >= 40) {
+      ;(textPrince.innerHTML = `Seu IMC é ${finishedResult}`),
+        (textSecondary.innerHTML = '(Obesidade grau 3)')
+    } else {
+      alert('Preencha com numeros')
+      return
+    }
+
+    if (valueWeight && valueHeight) {
+      modal.classList.toggle('open')
+    }
+
+    if (!valueWeight && !valueHeight) {
+      alert('Preencha os campos')
+      return
+    }
+  })
+
+  resetSite.addEventListener('click', () => {
+    document.location.reload(true)
+  })
+
+  closeModal.addEventListener('click', () => {
+    if (modal.classList.contains('open')) {
+      modal.classList.remove('open')
+    }
+  })
+}
+
+// ==================  EXECUTANDO AS FUNÇÕES =================
+
+interrogationInformation()
+themeSwitcher()
+receiveValueInputAndCalc()
